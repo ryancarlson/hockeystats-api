b67ef400112a8d1b31e59c411b51b42c8fca6ab6
ryancarlson <ryan.t.carlson@cru.org>
2015-05-04 22:41:20 -0400

    Add Joda time dependency

---
+++ src/main/java/org/carlson/hockeystats/domain/Game.java
@@ -0,0 +1,8 @@
+package org.carlson.hockeystats.domain;
+
+/**
+ * Created by ryancarlson on 5/4/15.
+ */
+public class Game
+{
+}
---
+++ src/test/java/org/carlson/hockeystats/service/TeamServiceTest.java
@@ -0,0 +1,8 @@
+package org.carlson.hockeystats.service;
+
+/**
+ * Created by ryancarlson on 5/4/15.
+ */
+public class TeamServiceTest
+{
+}
